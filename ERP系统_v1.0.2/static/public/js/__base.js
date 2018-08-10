509c06c410a5ec4460e1fb4ac11d3a643a536408
小伍 <35486827+wuguanyao@users.noreply.github.com>
2018-08-10 10:05:09 +0800

    更新主机名

--- ERP系统_v1.0.2/static/public/js/__base.js
+++ ERP系统_v1.0.2/static/public/js/__base.js
@@ -2,7 +2,7 @@
 
 window.__var = { //全局共用变量
 
-	baseURL : 'http://47.52.196.136:801/sys/'
+	baseURL : 'http://erp.bzttech.com/app.php/sys/'
 //	baseURL : 'http://192.168.2.200:801/sys/'
 	//完整路径 http://localhost:801/app.php/app模块/index控制器/xx方法 
 	
